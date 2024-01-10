082a2bac419d646c06049802272571ffc564d1d9
User <User@DESKTOP-4DVE6NI>
2024-01-10 08:39:50 +0300

    Добавьте файлы проекта.

---
+++ WebTechLabs/Services/DbInitializer.cs
@@ -0,0 +1,84 @@
+﻿using Microsoft.AspNetCore.Identity;
+using WebTechLabs.DAL.Data;
+using WebTechLabs.DAL.Entities;
+
+namespace WebTechLabs.Services
+{
+    public static class DbInitializer
+    {
+        public static async Task Seed(this WebApplication app)
+        {
+            using var scoped = app.Services.CreateScope();
+            using var context = scoped.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+            using var userManager = scoped.ServiceProvider.GetService<UserManager<ApplicationUser>>();
+            using var roleManager = scoped.ServiceProvider.GetRequiredService<RoleManager<IdentityRole>>();
+
+            // создать БД, если она еще не создана
+            context.Database.EnsureCreated();
+            // проверка наличия ролей
+            if (!context.Roles.Any())
+            {
+                var roleAdmin = new IdentityRole
+                {
+                    Name = "admin",
+                    NormalizedName = "admin"
+                };
+                // создать роль admin
+                await roleManager.CreateAsync(roleAdmin);
+            }
+            // проверка наличия пользователей
+            if (!context.Users.Any())
+            {
+                // создать пользователя user@mail.ru
+                var user = new ApplicationUser
+                {
+                    Email = "user@mail.ru",
+                    UserName = "user@mail.ru"
+                };
+                await userManager.CreateAsync(user, "123456");
+                // создать пользователя admin@mail.ru
+                var admin = new ApplicationUser
+                {
+                    Email = "admin@mail.ru",
+                    UserName = "admin@mail.ru"
+                };
+                await userManager.CreateAsync(admin, "123456");
+                // назначить роль admin
+                admin = await userManager.FindByEmailAsync("admin@mail.ru");
+                await userManager.AddToRoleAsync(admin, "admin");
+            }
+
+            if (!context.DishGroups.Any())
+            {
+                context.DishGroups.AddRange(
+                new List<DishGroup>
+                {
+                     new DishGroup { GroupName="Стартеры"},
+                     new DishGroup {GroupName="Салаты"},
+                     new DishGroup {GroupName="Супы"},
+                     new DishGroup {GroupName="Основные блюда"},
+                     new DishGroup {GroupName="Напитки"},
+                     new DishGroup {GroupName="Десерты"}
+                });
+                await context.SaveChangesAsync();
+            }
+
+            if (!context.Dishes.Any())
+            {
+                context.Dishes.AddRange(
+                new List<Dish>
+                {
+                    new Dish { DishName="Суп-харчо", Description="Очень острый, невкусный", Calories =200, DishGroupId=3, Image="1.png" },
+                    new Dish { DishName="Борщ", Description="Много сала, без сметаны", Calories =330, DishGroupId=3, Image="4.png" },
+                    new Dish { DishName="Суп 1", Description="", Calories =330, DishGroupId=3, Image="2.png" },
+                    new Dish { DishName="Суп 2", Description="", Calories =330, DishGroupId=3, Image="3.png" },
+                    new Dish { DishName="Котлета пожарская", Description="Хлеб - 80%, Морковь - 20%", Calories =635, DishGroupId=4, Image="Котлета.jpg" },
+                    new Dish { DishName="Макароны по-флотски", Description="С охотничьей колбаской", Calories =524, DishGroupId=4, Image="Макароны.jpg" },
+                    new Dish { DishName="Компот", Description="Быстро растворимый, 2 литра", Calories =180, DishGroupId=5, Image="Компот.jpg" }
+                });
+
+                await context.SaveChangesAsync();
+            }
+        }
+    }
+}
